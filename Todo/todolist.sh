cad1b191819171dfa83a8a98b5af74b402e704ac
BhupeshB7 <bhupeshkr2912@gmail.com>
2024-07-12 15:18:45 +0530

    update

--- Todo/todolist.sh
+++ Todo/todolist.sh
@@ -48,43 +48,67 @@ delete_task() {
     mv "$temp_file" "$FILE"
     echo "Task(s) deleted!"
 }
+
+while true; do
+    echo "-----------------"
+    echo " Todo List Tool  "
+    echo "-----------------"
+    echo "1. List all tasks"
+    echo "2. Display all tasks"
+    echo "3. Add a task"
+    echo "4. Display tasks by status(todo)"
+    echo "5. Display tasks by status(doing)"
+    echo "6. Display tasks by status(complete)"
+    echo "7. Mark a task as complete"
+    echo "8. Delete a task"
+    echo "9. Exit"
+
+    read -p "Enter your choice: " choice
+
 case $1 in
-        "")
+        1)
             display_by_status 0
           ;;
-        display)
+        2)
             displayTask
           ;;
-        add)
+        3)
             shift
             add_task "$*"
           ;;
-        todo)
+        4)
            display_by_status 0
           ;;
-        doing)
+        5)
            display_by_status 1
           ;;
-        complete)
+        6)
            display_by_status 2
           ;;
-        mark_complete)
+        7)
+        read -p "Enter task serial number(s) to mark as complete (space separated): " serial_numbers
            shift
-           if [[ $# -gt 0 && $# -le 8 ]];then
-           mark_as_complete "$*"
+           if [[ $serial_numbers -gt 0 && $serial_numbers -le 8 ]];then
+           mark_as_complete "$serial_numbers"
            else
                    echo "Error: You must provide between 1 and 8 serial number"
           fi
           ;;
-        delete)
+        8)
+        read -p "Enter task serial number(s) to mark as complete (space separated): " serial_numbers
            shift
-           if [[ $# -gt 0 && $# -le 8 ]]; then
-              delete_task "$*"
+           if [[ $serial_numbers -gt 0 && $serial_numbers -le 8 ]]; then
+              delete_task "$serial_numbers"
            else
                    echo "Error: You must provide b/w 1 and 8 serial number"
            fi
+          ;;
+        9)
+           echo "Exiting..."
+           exit 0
           ;;
         *)
            echo "Error; Invalid Argument , Please Enter valid"
           ;;
 esac
+done
